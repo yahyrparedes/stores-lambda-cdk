11b9f13904f196ec9fcb069cd6e744be37e42630
Yahyr Enrique Paredes Arteaga <yahyrparedesarteaga@gmail.com>
2024-08-23 11:57:28 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
-# Welcome to your CDK TypeScript project
+# CDK Example Project using Lambda, API Gateway and DynamoDB
+
+This is a simple example project to demonstrate the use of the AWS Cloud Development Kit (CDK)
+to create a simple aws-lambda function and an API Gateway to trigger it.
+
+![infra.png](infra.png)
+
+## Useful paths
+
+| Method | Path       | Body         | Func Name | Description       |
+|--------|------------|--------------|-----------|-------------------|
+| GET    | /store/:id |              | find      | Search item by id |
+| GET    | /stores    |              | list      | Search all items  |
+| POST   | /store     | {name: Lima} | save      | Create one item   |
+| DELETE | /store     | {name: Lima} | delete    | Remove one item   |
+
+> Default CDK project README.md
+
+## Welcome to your CDK TypeScript project
 
 This is a blank project for CDK development with TypeScript.
 
 The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
-## Useful commands
+### Useful commands
 
 * `npm run build`   compile typescript to js
 * `npm run watch`   watch for changes and compile
@@ -12,3 +30,4 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npx cdk deploy`  deploy this stack to your default AWS account/region
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
+
--- lib/stores-lambda-stack.ts
+++ lib/stores-lambda-stack.ts
@@ -1,16 +1,87 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as path from "path";
 
-export class StoresLambdaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class StoresLambdaStack extends Stack {
 
-    // The code that defines your stack goes here
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'StoresLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // DybanmoDB table stores
+        const tableStore = new dynamodb.Table(this, 'StoreTable', {
+            partitionKey: {name: 'id', type: dynamodb.AttributeType.STRING},
+            tableName: 'InfrastructureTable',
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+
+        // Lambda function GET
+        const getLambdaStores = new lambda.Function(this, 'ListLambdaStore', {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            handler: 'handlers.list',
+            code: lambda.Code.fromAsset(path.resolve(__dirname, 'stores')),
+            environment: {
+                TABLE_NAME: tableStore.tableName
+            }
+        });
+
+        // Lambda function POST
+        const postLambdaStore = new lambda.Function(this, 'SaveLambdaStore', {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            handler: 'handlers.save',
+            code: lambda.Code.fromAsset(path.resolve(__dirname, 'stores')),
+            environment: {
+                TABLE_NAME: tableStore.tableName
+            }
+        });
+
+
+        // Lambda function GET
+        const findLambdaStore = new lambda.Function(this, 'FindLambdaStore', {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            handler: 'handlers.find',
+            code: lambda.Code.fromAsset(path.resolve(__dirname, 'stores')),
+            environment: {
+                TABLE_NAME: tableStore.tableName
+            }
+        });
+
+        // Lambda function DELETE
+        const deleteLambdaStore = new lambda.Function(this, 'DeleteLambdaStore', {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            handler: 'handlers.delete',
+            code: lambda.Code.fromAsset(path.resolve(__dirname, 'stores')),
+            environment: {
+                TABLE_NAME: tableStore.tableName
+            }
+        });
+
+        // permission to lambda to access dynamodb table
+        tableStore.grantReadWriteData(postLambdaStore);
+        tableStore.grantReadWriteData(deleteLambdaStore);
+        tableStore.grantReadData(getLambdaStores);
+        tableStore.grantReadData(findLambdaStore);
+
+        // create api gateway and connect to lambda
+        const apigate = new apigw.RestApi(this, 'ApiGatewayStore');
+        apigate.root
+            .resourceForPath('stores')
+            .addMethod('GET', new apigw.LambdaIntegration(getLambdaStores));
+
+        apigate.root
+            .resourceForPath('stores')
+            .addMethod('POST', new apigw.LambdaIntegration(postLambdaStore));
+
+        apigate.root
+            .resourceForPath('store')
+            .addMethod('GET', new apigw.LambdaIntegration(findLambdaStore));
+
+        apigate.root
+            .resourceForPath('store')
+            .addMethod('DELETE', new apigw.LambdaIntegration(deleteLambdaStore));
+
+    }
 }
